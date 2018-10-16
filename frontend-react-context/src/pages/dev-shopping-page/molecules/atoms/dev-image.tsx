ec15c0caa4a3f76ac540375feacfb3ca227b2750
Igor Fischer <igor.fischer154@gmail.com>
2018-10-16 23:51:29 +0200

    just use top margin for image

--- frontend-react-context/src/pages/dev-shopping-page/molecules/atoms/dev-image.tsx
+++ frontend-react-context/src/pages/dev-shopping-page/molecules/atoms/dev-image.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 export default (props) => <img {...props} style={{
 	maxWidth: 160,
 	boxShadow: '4px 3px 3px black',
-	margin: '20px',
+	marginRight: '20px',
 	border: '1px solid grey',
 }} />;
